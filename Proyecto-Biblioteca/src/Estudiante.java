755b88024a7b93345f68cb90e88b6cb6f42df0bb
hurtadoalejo <alejohg2911@gmail.com>
2024-09-21 13:56:39 -0500

    Esta vuelta está extraña

--- Proyecto-Biblioteca/src/Estudiante.java
+++ Proyecto-Biblioteca/src/Estudiante.java
@@ -1,8 +1,5 @@
-public class Estudiante {
-    private String nombre;
-    private String cedula;
-    private String telefono;
-    private String correo;
+public class Estudiante extends Persona{
+
 
     /**
      * Metodo constructor de la clase Estudiante
@@ -12,75 +9,14 @@ public class Estudiante {
      * @param correo Correo del estudiante a crear
      */
     public Estudiante(String nombre, String cedula, String telefono, String correo) {
-        this.nombre = nombre;
-        this.cedula = cedula;
-        this.telefono = telefono;
-        this.correo = correo;
-    }
-
-    /**
-     * Metodo para obtener el nombre del estudiante
-     * @return Nombre del estudiante
-     */
-    public String getNombre() {
-        return nombre;
-    }
-    /**
-     * Metodo para obtener la cedula del estudiante
-     * @return Cedula del estudiante
-     */
-    public String getCedula() {
-        return cedula;
-    }
-    /**
-     * Metodo para obtener el telefono del estudiante
-     * @return Telefono del estudiante
-     */
-    public String getTelefono() {
-        return telefono;
-    }
-    /**
-     * Metodo para obtener el correo del estudiante
-     * @return Correo del estudiante
-     */
-    public String getCorreo() {
-        return correo;
-    }
-
-    /**
-     * Metodo para modificar el nombre del estudiante
-     * @param nombre Nuevo nombre del estudiante
-     */
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    /**
-     * Metodo para modificar el nombre del estudiante
-     * @param nombre Nuevo nombre del estudiante
-     */
-    public void setCedula(String cedula) {
-        this.cedula = cedula;
-    }
-    /**
-     * Metodo para modificar el nombre del estudiante
-     * @param nombre Nuevo nombre del estudiante
-     */
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-    /**
-     * Metodo para modificar el nombre del estudiante
-     * @param nombre Nuevo nombre del estudiante
-     */
-    public void setCorreo(String correo) {
-        this.correo = correo;
+        super(nombre, cedula, telefono, correo);
     }
 
     /**
      * Metodo para obtener la informacion del estudiante
      * @return Informacion del estudiante
      */
-    public String toString() {
-        return "Estudiante: nombre=" + nombre + ", cedula=" + cedula + ", telefono=" + telefono + ", correo=" + correo;
+    public String toString(){
+        return super.toString();
     }
 }
